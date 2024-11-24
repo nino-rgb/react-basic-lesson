2ea95581f59995ea0b3c5543bcaa25dd72564c83
nino-rgb <pnekoneko907@gmail.com>
2024-11-24 16:04:54 +0900

    add loop,if

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import { TEST } from './Props';
 import { useState } from 'react';
+import { Count } from './model/count';
+import { idText, readBuilderProgram } from 'typescript';
 
 function App() {
   const test:string = 'Hello, World!';
@@ -10,13 +12,43 @@ function App() {
   const onstate = () => {
    setState(state + 1);
   console.log(state);
-  }
+  setCount(true);
+  const newLoopHello = [...loopHello];
+  newLoopHello.push({id:state, hello:'Hello'});
+  setLoopHello(newLoopHello);
+  console.log (loopHello);  
+}
+
+　//条件文用
+  const [count, setCount] = useState(false); 
+
+  // ループ文用
+  const [loopHello, setLoopHello] = useState<Count[]>([]); 
 
   return (
     <div className="App">
      <TEST text={test}/>
      <button onClick={onstate}>{state}</button>
-    </div>
+     {/* 条件文 */}
+    {/* {(() => {if(count === true){
+      return <div>count UP!</div>
+    }else{
+      return null;
+    }})()} */}
+    {count && <div>count UP!</div>}
+
+{/* ループ文 */}
+{/* {(() => {
+  const showLoopHello = [];
+  for (let i =0; i< state; i++) {
+  showLoopHello.push(<div key={i}>{loopHello[i].hello}</div>);
+}
+ return showLoopHello;
+})()} */}
+{loopHello.map((count:Count) => {
+  return <div key={count.id}>{count.hello}</div>
+  })}
+</div>
   );
 }
 
---
+++ src/model/count.ts
@@ -0,0 +1,4 @@
+export type Count = {
+    id?:number
+    hello:string
+}
